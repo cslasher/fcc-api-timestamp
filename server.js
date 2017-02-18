20b7e6d82436c597614c3afac6a42908eeb49f51
Edwin L <cslasher@gmail.com>
2017-02-18 07:11:30 +0000

    Setup functions

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 var express = require('express')
-
+var locale = "en-us"
 var app = express()
+var json = {
+    unix: null,
+    natural: null
+}
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:timeParam', function (req, res) {
+    // if (dateTime/1000 > 0) {
+    //   json = {
+    //       unix: dateTime/1000,
+    //       natural: dateTime.toLocaleString(locale, { month: "long" }) + " " + dateTime.getDay() + ", " + dateTime.getFullYear()
+    //   }
+    // } 
+    
+    // console.log("json:" + json)
+    // res.end(req.params.timeParam, function(data) {
+    //     var dateTime = Date(data)
+    // if (dateTime/1000 > 0) {
+    //   json = {
+    //       unix: dateTime/1000,
+    //       natural: dateTime.toLocaleString(locale, { month: "long" }) + " " + dateTime.getDay() + ", " + dateTime.getFullYear()
+    //   }
+    // }         
+    // })
+    res.end(req.params.timeParam)
 })
 
 app.listen(process.env.PORT, function () {
-  console.log(`Example app listening on port ${process.env.PORT}!`)
+    console.log(`Timestamp app listening on port ${process.env.PORT}!`)
 })
