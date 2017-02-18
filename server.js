41cda64d2e808a511ba6a3e3b54ecd81be65b639
Edwin Liu <edwinliu@ip-10-11-14-70.ap-northeast-1.compute.internal>
2017-02-18 17:47:39 +0800

    Add timestamp function

--- server.js
+++ server.js
@@ -2,31 +2,47 @@ var express = require('express')
 var locale = "en-us"
 var app = express()
 var json = {
-    unix: null,
-    natural: null
+  unix: null,
+  natural: null
 }
 
-app.get('/:timeParam', function (req, res) {
-    // if (dateTime/1000 > 0) {
-    //   json = {
-    //       unix: dateTime/1000,
-    //       natural: dateTime.toLocaleString(locale, { month: "long" }) + " " + dateTime.getDay() + ", " + dateTime.getFullYear()
-    //   }
-    // } 
-    
-    // console.log("json:" + json)
-    // res.end(req.params.timeParam, function(data) {
-    //     var dateTime = Date(data)
-    // if (dateTime/1000 > 0) {
-    //   json = {
-    //       unix: dateTime/1000,
-    //       natural: dateTime.toLocaleString(locale, { month: "long" }) + " " + dateTime.getDay() + ", " + dateTime.getFullYear()
-    //   }
-    // }         
-    // })
-    res.end(req.params.timeParam)
+app.get('/', function (req, res) {
+  res.end(JSON.stringify(json))
 })
 
-app.listen(process.env.PORT, function () {
-    console.log(`Timestamp app listening on port ${process.env.PORT}!`)
+app.get('/:timeParam', function (req, res) {
+  // if (dateTime/1000 > 0) {
+  //   json = {
+  //       unix: dateTime/1000,
+  //       natural: dateTime.toLocaleString(locale, { month: "long" }) + " " + dateTime.getDay() + ", " + dateTime.getFullYear()
+  //   }
+  // }
+
+  // console.log("json:" + json)
+  // res.end(req.params.timeParam, function(data) {
+  //     var dateTime = Date(data)
+  // if (dateTime/1000 > 0) {
+  //   json = {
+  //       unix: dateTime/1000,
+  //       natural: dateTime.toLocaleString(locale, { month: "long" }) + " " + dateTime.getDay() + ", " + dateTime.getFullYear()
+  //   }
+  // }
+  // })
+  var time = req.params.timeParam
+  res.end(naturalTime(time))
 })
+
+app.listen(process.env.PORT|3000, function () {
+  console.log(`Timestamp app listening on port ${process.env.PORT|3000}!`)
+})
+function naturalTime(time) {
+  if (isNaN(time)) {
+    dateTime = new Date(Date.parse(time))
+  } else {
+    dateTime = new Date(time * 1000)
+  }
+
+  json.unix = dateTime.getTime() / 1000
+  json.natural = dateTime.toLocaleString(locale, { month: "long" }) + " " + dateTime.getDay() + ", " + dateTime.getFullYear()
+  return JSON.stringify(json)
+}
